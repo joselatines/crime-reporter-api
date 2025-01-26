458f90be498b7a597d556d5a2d4a3ed29c2f15b8
caladavid <davidcalae@gmail.com>
2025-01-26 18:07:24 -0400

    test cors

--- src/app.ts
+++ src/app.ts
@@ -12,24 +12,14 @@ import MessageResponse from './interfaces/MessageResponse';
 const app = express();
 dotenv.config();
 
-const allowedOrigins = ["http://localhost:4200", "https://crime-reporter-lime.vercel.app", "http://localhost:5000"];
-
-const corsOptions = {
-  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
-    if (allowedOrigins.indexOf(origin || '') !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization', 'cache-control'],
-};
-  
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'http://localhost:4200', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization', 'cache-control'],
+  credentials: true,
+}));
 /* app.use(cors({
   origin: 'https://crime-reporter-lime.vercel.app', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
